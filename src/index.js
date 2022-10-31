12af4ff6d7cfa41b77359ebc06b9734c670fe265
Eddahkarambu <gikundaeddah@gmail.com>
2022-10-31 14:12:15 +0300

    merged changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
 
+const Store = createStore();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
