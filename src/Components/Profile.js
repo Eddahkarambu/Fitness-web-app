311f4c4253b44a7448aad0e76543b79cbbffeb03
Eddahkarambu <gikundaeddah@gmail.com>
2022-11-22 14:26:55 +0300

    add a circle

---
+++ src/Components/Profile.js
@@ -0,0 +1,18 @@
+import React  from "react";
+import './Profile.css';
+import Navbar from './Navbar.js';
+
+
+
+function Profile(){
+    return(
+        <div className="Circle">
+
+        </div>
+
+    )
+
+}
+
+
+export default Profile
