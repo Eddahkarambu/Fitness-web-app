c450459ad3deedd310ef2e4a845ee16af7f9a571
Eddahkarambu <gikundaeddah@gmail.com>
2022-12-06 13:33:18 +0300

    implement redux in users

--- src/Redux/Actions/user.actions.js
+++ src/Redux/Actions/user.actions.js
@@ -1,5 +1,28 @@
-// import { userConstants } from "../_constants";
+import axios from "axios";
+import { userActions } from "../Constants";
 
-export const userActions = {
-  //    Signup
+function request() {
+  return { type: userActions.REQUEST };
+}
+
+function success(message) {
+  return { type: userActions.SUCCESS, message };
+}
+
+function failure(message) {
+  return { type: userActions.FAILURE, message };
+}
+
+export const authenticateUser = (userDetails) => {
+  return (dispatch) => {
+    dispatch(request());
+    axios
+      .post(`http://localhost:3001/users/login`, { ...userDetails })
+      .then(function (res) {
+        dispatch(success(res));
+      })
+      .catch(function (err) {
+        dispatch(failure(err));
+      });
+  };
 };
