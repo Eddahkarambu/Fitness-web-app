37df47bdbc954ea199384ce89f35dc25fc44ef30
Eddahkarambu <gikundaeddah@gmail.com>
2023-07-26 17:11:42 +0300

    add a registartion form

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Services.css";
 import apple from "../Images/apple fitness.jpg";
+import greatcoaching from "../Images/greatcoaching.png";
+import groupworkout from "../Images/groupworkout.webp";
 
 function Services() {
   return (
@@ -70,14 +72,95 @@ function Services() {
         <img className="apple" src={apple} alt="apple" />
       </div>
       <div className="Group">
-        <h1></h1>
+        <div className="allcommunity">
+          <div>
+            {" "}
+            <img
+              className="groupworkout"
+              src={groupworkout}
+              alt="groupworkout"
+            />
+          </div>
+          <div className="communitypara">
+            <h1>GROUP CLASSES AND COMMUNITY</h1>
+            <p>
+              Our experience is that beginners and seasoned athletes rise to the
+              occasion, are more energized, and produce longer-lasting results
+              when they're surrounded by a community that encourages and
+              empowers them through every workout. That's why our FPF Core
+              Programs and Specialty Classes are intentionally about
+              Togetherness.
+            </p>
+            <p>
+              While everyone's goals may differ, the bottom line is, if fitness
+              is fun and personal connections are meaningful, your motivation is
+              going to increase. If you have a WHY, our group classes are the
+              HOW that gets you where you want to be.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="Groupp">
+        <div className="allcommunityy">
+          <div className="communitypara">
+            <h1>GREAT COACHING</h1>
+            <p>
+              FirstPower is proud to set itself apart as a gym where all of our
+              coaches are highly trained and internationally certified. Our
+              training staff is committed to giving you the attention you need
+              with a high standard for excellence in the form, function and
+              execution of every movement, but we also are passionate about
+              making fitness a great experience.
+            </p>
+            <p>
+              Striving hard after your goals should be something you look
+              forward to day after day, and our coaching and FPF Core Programs,
+              Specialty Classes and Skill Sessions will deliver!
+            </p>
+          </div>
+          <div>
+            {" "}
+            <img
+              className="greatcoaching"
+              src={greatcoaching}
+              alt="greatcoaching"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="register">
+        <div className="reg">
+          <div className="registry">REGISTRATION FORM</div>
+          <div className="registerperson">Register in person</div>
+          <div className="completeregistry">
+            ...or download, complete, and email your registration to
+            lauren@firstpowerfitness.com. Call +254 799159971 to have any
+            questions answered!
+          </div>
+        </div>
+      </div>
+
+      <div className="formdetail">
+        <div className="questions">Have Any Questions?</div>
+        <div className="question1">
+          Fill out the form to get in touch with us.
+        </div>
+        <div className="list">
+          <ul>
+            Nairobi, Kenya
+            <li>Galleria Shopping Mall</li>
+            <li>Langata Rd/Magadi Rd Junction</li>
+            <li>Nairobi, Kenya</li>
+            <li>PO Box 2224-00502</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
 }
 export default Services;
-
-// https://firstpowerfitness.com/get-started/
 
 // import ladies from "../Images/ladies.jpg";
 // import mens from "../Images/mens.jpg";
