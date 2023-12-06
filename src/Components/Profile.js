ce0325b11aa3173086e7c00f3a82cc9ab0d2f2da
Eddahkarambu <gikundaeddah@gmail.com>
2023-12-06 16:24:57 +0300

    add the profile page

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,39 +1,112 @@
-import React from "react";
-import "./Profile.css";
+// src/components/UserProfile.js
 
-function Profile() {
+import React, { useState } from "react";
+import "./UserProfile.css";
+
+const UserProfile = () => {
+  const [editing, setEditing] = useState(false);
+  const [userData, setUserData] = useState({
+    username: "Eddah",
+    fullName: "Eddah Karambu",
+    age: 25,
+    gender: "Female",
+  });
+
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleSaveClick = () => {
+    setEditing(false);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
   return (
-    <div>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
-      <div className="card">
-        <h1>Eddah Karambu</h1>
-        <p className="title">CEO &amp; Founder</p>
-        <p>CROSSFIT COLLECTIVE</p>
+    <div className="UserProfile">
+      <header>
+        <h1>User Profile</h1>
+      </header>
 
-        <a href="#">
-          <i className="fa fa-dribbble" />
-        </a>
+      <main>
+        <section className="user-info">
+          <h2>User Information</h2>
+          {editing ? (
+            <form>
+              <label>
+                Username:
+                <input
+                  type="text"
+                  name="username"
+                  value={userData.username}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Full Name:
+                <input
+                  type="text"
+                  name="fullName"
+                  value={userData.fullName}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Age:
+                <input
+                  type="number"
+                  name="age"
+                  value={userData.age}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Gender:
+                <select
+                  name="gender"
+                  value={userData.gender}
+                  onChange={handleChange}
+                >
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                  <option value="Other">Other</option>
+                </select>
+              </label>
+            </form>
+          ) : (
+            <div>
+              <p>
+                <strong>Username:</strong> {userData.username}
+              </p>
+              <p>
+                <strong>Full Name:</strong> {userData.fullName}
+              </p>
+              <p>
+                <strong>Age:</strong> {userData.age}
+              </p>
+              <p>
+                <strong>Gender:</strong> {userData.gender}
+              </p>
+            </div>
+          )}
+        </section>
 
-        <a href="#">
-          <i className="fa fa-twitter" />
-        </a>
-
-        <a href="#">
-          <i className="fa fa-linkedin" />
-        </a>
-
-        <a href="#">
-          <i className="fa fa-facebook" />
-        </a>
-
-        <p>
-          <button>Contact</button>
-        </p>
-      </div>
+        <section className="buttons">
+          {editing ? (
+            <button onClick={handleSaveClick}>Save Changes</button>
+          ) : (
+            <button onClick={handleEditClick}>Edit Profile</button>
+          )}
+        </section>
+      </main>
     </div>
   );
-}
-export default Profile;
+};
+
+export default UserProfile;
