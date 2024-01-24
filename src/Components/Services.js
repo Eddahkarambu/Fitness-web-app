7952a02d0e781d6ffb88a8a1104d23d9fd04ac01
MuthomiMate <muthomimate@gmail.com>
2024-01-24 14:40:06 +0300

    install the font awesome icons

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -144,92 +144,14 @@ function Services() {
         </div>
       </div>
 
-      <div className="formdetail">
-        <div className="list">
-          <div className="listItem">Nairobi, Kenya</div>
-          <div className="listItem">Galleria Shopping Mall</div>
-          <div className="listItem">Langata Rd/Magadi Rd Junction</div>
-          <div className="listItem">Nairobi, Kenya</div>
-          <div className="listItem">PO Box 2224-00502</div>
-        </div>
+      <div className="list">
+        <div className="listItem">Nairobi, Kenya</div>
+        <div className="listItem">Galleria Shopping Mall</div>
+        <div className="listItem">Langata Rd/Magadi Rd Junction</div>
+        <div className="listItem">Nairobi, Kenya</div>
+        <div className="listItem"> PO Box 2224-00502</div>
       </div>
     </div>
   );
 }
 export default Services;
-
-// import ladies from "../Images/ladies.jpg";
-// import mens from "../Images/mens.jpg";
-// import nutrition from "../Images/nutrition.jpg";
-// import online from "../Images/online.jpg";
-// function Services() {
-//   return (
-//     <div className="Serve">
-//       {" "}
-//       <div className="fitnessheader">FITNESS EXERCISES</div>
-//       <div className="service">
-//         <div>
-//           <img className="ladies" src={ladies} alt="ladies" />
-//         </div>
-//         <h1 className="headings">Aerobics</h1>
-//         <p>
-//           Aerobics is a form of physical exercise that combines rhythmic aerobic
-//           exercise with stretching and strength training routines with the goal
-//           of improving all elements of fitness (flexibility, muscular strength,
-//           and cardio-vascular fitness).
-//         </p>
-//       </div>
-//       <div className="service">
-//         <div>
-//           <img className="online" src={online} alt="online" />
-//         </div>
-//         <h1 className="headings">Pilates exercise</h1>
-//         <p>
-//           Pilates is a type of exercise that focuses on improving flexibility,
-//           strength, and body awareness through controlled movements and was
-//           created in the 1920s by German physical trainer Joseph Pilates for
-//           rehabilitation purposes. His method was influenced by other forms of
-//           exercise, including gymnastics and boxing.
-//         </p>
-//       </div>
-//       <div className="service">
-//         <div>
-//           <img className="mens" src={mens} alt="mens" />
-//         </div>
-//         <h1 className="headings">Circuit Training</h1>
-//         <p>
-//           Circuit training is a form of body conditioning that involves
-//           endurance training, resistance training, high-intensity aerobics, and
-//           exercises performed in a circuit, similar to high-intensity interval
-//           training. It targets strength building and muscular endurance.
-//         </p>
-//       </div>
-//       <div className="service">
-//         <div>
-//           <img className="nutrition" src={nutrition} alt="nutrition" />
-//         </div>
-//         <h1 className="headings">Cardio</h1>
-//         <p>
-//           Aerobic exercise is physical exercise of low to high intensity that
-//           depends primarily on the aerobic energy-generating process. "Aerobic"
-//           is defined as "relating to, involving, or requiring oxygen", and
-//           refers to the use of oxygen to meet energy demands during exercise via
-//           aerobic metabolism adequately.
-//         </p>
-//       </div>
-//       <div className="programs">
-//         <div className="programs-header">
-//           <span>Explore our</span>
-//           <span>programs</span>
-//           <span>To shape you</span>
-//         </div>
-//       </div>
-//       <div className="program-categories">
-//         <span></span>
-//         <span></span>
-//         <span></span>
-//       </div>
-//     </div>
-//   );
-// }
-// export default Services;
