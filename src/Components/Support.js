961e177544f55b9102ad2fba7b4e6a5cd2b854e5
MuthomiMate <muthomimate@gmail.com>
2024-02-08 16:42:23 +0300

    add support paragraph

--- src/Components/Support.js
+++ src/Components/Support.js
@@ -8,6 +8,14 @@ function Support() {
 
       <div className="Support-programs">
         <div className="support">NEED SUPPORT WE ARE HERE FOR YOU</div>;
+        <div className="problem1">
+          If you are having problems with our app or services, we’re here to
+          help!
+        </div>
+        <div className="problem2">
+          Visit our Help Desk to browse our user guides and frequently asked
+          questions:
+        </div>
       </div>
     </div>
   );
